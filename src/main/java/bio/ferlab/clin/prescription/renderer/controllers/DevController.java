3fa99394916522e6affd9403520358302b55b709
Yann MARCOU <creativeyann17@gmail.com>
2022-03-08 15:23:04 -0500

    feat: CLIN-837 - local trustStore + dockerfile

--- src/main/java/bio/ferlab/clin/prescription/renderer/controllers/DevController.java
+++ src/main/java/bio/ferlab/clin/prescription/renderer/controllers/DevController.java
@@ -1,32 +1,24 @@
 package bio.ferlab.clin.prescription.renderer.controllers;
 
-import bio.ferlab.clin.prescription.renderer.clients.FhirAsyncClient;
-import bio.ferlab.clin.prescription.renderer.clients.FhirClient;
-import bio.ferlab.clin.prescription.renderer.exceptions.RenderException;
-import bio.ferlab.clin.prescription.renderer.models.*;
-import bio.ferlab.clin.prescription.renderer.services.PdfService;
-import bio.ferlab.clin.prescription.renderer.services.ResourceService;
-import bio.ferlab.clin.prescription.renderer.services.SecurityService;
-import bio.ferlab.clin.prescription.renderer.services.ThymeleafService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 @Profile("dev")
 @Controller
 public class DevController {
+
+  static {
+    System.setProperty("javax.net.ssl.trustStore", "cacerts.jks");
+    System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+  }
   
   @Autowired
   private PdfController pdfController;
